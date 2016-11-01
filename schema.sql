0227a96afeaf03ce39a1a1a0bc83ada3b88c230f
Jason Danker <jason.danker@ischool.berkeley.edu>
2016-10-31 17:26:27 -0700

    initial schema

---
+++ schema.sql
@@ -0,0 +1,58 @@
+drop table if exists users;
+create table users (
+	user_id integer primary key,
+	first_name text null null,
+	last_name text not null,
+	email text not null unique,
+	insecure_password text not null,
+	active boolean not null
+);
+
+drop table if exists trips;
+create table trips (
+	trip_id integer primary key,
+	budget integer not null,
+	date_outbound date not null,
+	date_inbound date not null,
+	flight_outbound integer,
+	flight_inbound integer,
+	hotel integer,
+	destination text,
+	budget_remaining integer not null, -- Calculated
+	active boolean not null
+);
+
+drop table if exists flights;
+create table flights (
+	flight_id integer primary key,
+	airline text not null,
+	flight_number text not null,
+	flight_date date not null,
+	origin text not null,
+	destination text not null,
+	cost integer not null,
+	foreign key (trip_id) references trips(trip_id),
+	active boolean not null
+);
+
+drop table if exists hotels;
+create table hotels (
+	hotel_id integer primary key,
+	name text not null,
+	check_in date not null,
+	check_out date not null,
+	location text not null, -- Likely to be expanded
+	rating integer,
+	foreign key (trip_id) references trips(trip_id),
+	active boolean not null
+);
+
+drop table if exists user_trips_junct;
+create table user_trips_junct (
+	relation_id integer primary key,
+	user_id integer,
+	trip_id integer,
+	foreign key (user_id) references users(user_id),
+	foreign key (trip_id) references trips(trip_id),
+	active boolean not null
+);
