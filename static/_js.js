06f9bcc9d31924273b4d9441f5b30d362ddbfc11
‘sgvolkov’ <svolkov@ischool.berkeley.edu>
2016-11-04 13:42:36 -0700

    adding some frontend

---
+++ static/_js.js
@@ -0,0 +1,16 @@
+ // $(document).ready(function() {
+ //    $('select').material_select();
+ //  });
+
+// $(document).ready(
+//     $("#remove").on('click', "button", function() {
+//         // remove item
+//             if (!confirm("Are you sure you want to delete this trip?")){
+//               return false;
+//             }
+//             //added confirmation box before deleting trip
+//         ($(this).html("Remove this trip"));
+//         var removeTrip = $(this).parent();
+//         $(trip_id).remove();
+
+
---
+++ static/createtrip.js
@@ -0,0 +1,13 @@
+ $(document).ready(function() {
+    $('select').material_select();
+
+  //   $(".remove").on('click', "button", function() {
+  //       // remove item
+  //           if (!confirm("Are you sure you want to delete this trip?")){
+  //             return false;
+  //           }
+  //           //added confirmation box before deleting trip
+  //       ($(this).html("Remove this trip"));
+  //       var removeTrip = $(this).parent();
+  //       $(trip_id).remove()});
+  // });
